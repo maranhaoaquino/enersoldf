fc4052933d6db24b13164c108a7af8e4ae6ef07c
maranhaoaquino <maranhaoaquino@gmail.com>
2023-06-06 21:56:59 -0300

    ajuste sigle serviço

--- cmb2/single-servicos.php
+++ cmb2/single-servicos.php
@@ -9,13 +9,27 @@ function cmb2_fields_single_service() {
     ]);
 
     $cmb->add_field([
-        'name' => 'Imagem Destacada',
+        'name' => 'Imagem Topo',
         'id' => 'img_destaque',
         'type' => 'file',
         'options' => [
             'url' => false,
         ],
     ]);
+
+    $cmb->add_field( array(
+        'name' => 'Carrossel',
+        'desc' => '',
+        'id'   => 'thumb_servico',
+        'type' => 'file_list',
+        'text' => array(
+            'add_upload_files_text' => 'Adicionar Imagens', // default: "Add or Upload Files"
+            'remove_image_text' => 'Replacement', // default: "Remove Image"
+            'file_text' => 'Replacement', // default: "File:"
+            'file_download_text' => 'Replacement', // default: "Download"
+            'remove_text' => 'Replacement', // default: "Remove"
+        ),
+    ) );
 
     $cmb->add_field( array(
         'name' => 'Chamada para orçamento',
---
+++ content-servico.php
@@ -0,0 +1,92 @@
+<section>
+    <img src="<?php the_field_cmb2('img_destaque'); ?>" alt="">
+</section>
+<section class="container my-5">
+    <div class="row gy-4">
+        <div class="col-12 text-center">
+            <h1><?php the_title(); ?></h1>
+        </div>
+        <div class="col-12">
+            <?php the_content(); ?>
+        </div>
+        <div class="col-12">
+            <div class="row gy-4">
+                <div class="col-12 col-md-6">
+                    <div id="slide-servico" class="owl-carousel owl-theme">
+                        <div class="item">
+                            <img src="<?php the_post_thumbnail_url(); ?>">
+                            <?php $thumbs = get_field_cmb2('thumb_servico'); ?>
+                            <?php if(!empty($thumbs)){foreach($thumbs as $thumb){ ?>
+                                <div class="item">
+                                    <img src="<?php echo $thumb; ?>">
+                                </div>
+                            <?php }} ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <p><?php the_field_cmb2('destaque_textarea'); ?></p>
+                </div>
+            </div>
+        </div>
+        <?php
+            $args = array (
+                'post_type' => 'servico',
+                'orderby' => 'ID',
+                'order'   => 'DESC',
+                'nopaging' => false,
+                'posts_per_page' => '12',
+                'post_status' => 'publish'
+            );
+            $the_query = new WP_Query ( $args );
+        ?>
+        <div class="col-12">
+            <div class="row gy-3">
+                <div class="col-12 text-center">
+                    <h2>Conheça nossos serviços</h2>
+                </div>
+                <div class="owl-carousel owl-theme slide--card">
+                    <?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                        <div class="item">
+                            <?php include(TEMPLATEPATH . "/snippets/card.php"); ?>
+                        </div>
+                    <?php endwhile; else: endif; ?>
+                    <?php wp_reset_query(); wp_reset_postdata(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<script>
+    $('#slide-servico').owlCarousel({
+        loop:false,
+        nav:true,
+        items: 1,
+        dots: true,
+        video: true,
+        merge:true,
+        navText: [
+            "<i class='fa fa-chevron-left'></i>",
+            "<i class='fa fa-chevron-right'></i>"
+        ],
+    });
+
+    $('.slide--card').owlCarousel({
+        loop:false,
+        nav:true,
+        responsive:{
+            0:{
+                items:2,
+                margin: 10
+            },
+            712:{
+                items:3,
+                margin: 20
+            },
+            1023:{
+                items:3,
+                margin: 30
+            }
+        }
+    });
+</script>
